04d64671d1d2729449b880ee803cafaeb4305d1b
Shatha Makhlouf <shada_m2006@yahoo.com>
2021-04-10 17:49:03 +0300

    Fix: Image load error in bookItem

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,13 +1,21 @@
 import { Link } from 'react-router-dom'
+import defaultImg from '../img/default.jpg'
+import { useState } from 'react';
 
 const BookItem = ({ book, details, id }) => {
+    const [imageLoadError, setImageLoadError] = useState(true);
     // console.log(id);
     return (
         <Link to='/details' onClick={() => details(id)}>
             <div className='card'>
                 <div className='card-inner'>
                     <div className='card-front'>
-                        <img src={`https://reststop.randomhouse.com/resources/titles/${book.isbn}`} onError={(e) => { e.target.onerror = null; e.target.src = "../img/default.jfif" }} alt='Book' />
+                        <img src={`https://reststop.randomhouse.com/resources/titles/${book.isbn}`} onError={(e) => {
+                            if (imageLoadError) {
+                                setImageLoadError(false)
+                                e.target.src = defaultImg;
+                            }
+                        }} alt='Book' />
                     </div>
                     <div className='card-back'>
                         <h1>{book.titleweb}</h1>
